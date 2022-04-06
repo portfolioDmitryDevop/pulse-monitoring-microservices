dd2e495281aba106d2ca39bc125b38e32559cb4e
DimaTch <4dnik@mail.ru>
2022-04-06 12:16:33 +0300

    adds subscribers

---
+++ pulse-monitoring-jumps-notifier/src/test/java/telran/pulse/monitoring/jumps/notifier/PulseMonitoringJumpsNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.pulse.monitoring.jumps.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseMonitoringJumpsNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ pulse-monitoring-jumps-populator/src/test/java/telran/pulse/monitoring/jumps/populator/PulseMonitoringJumpsPopulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.pulse.monitoring.jumps.populator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseMonitoringJumpsPopulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
